d2504fcf0c94497cbe2bbcffb60c08c9779ba361
Yue Zhang <zhyue05@gmail.com>
2020-02-29 17:40:31 -0500

    add sign up constrains

--- Expirationtracker/app/src/main/java/com/example/expirationtracker/RegisterFragment.java
+++ Expirationtracker/app/src/main/java/com/example/expirationtracker/RegisterFragment.java
@@ -1,22 +1,38 @@
 package com.example.expirationtracker;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.expirationtracker.model.User;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RegisterFragment extends Fragment {
-
-
+public class RegisterFragment extends Fragment implements View.OnClickListener{
+    private EditText mEtUsername;
+    private EditText mEtPassword;
+    private EditText mEtConfirm;
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
     public RegisterFragment() {
         // Required empty public constructor
     }
@@ -28,7 +44,76 @@ public class RegisterFragment extends Fragment {
         Log.i(getString(R.string.register_frg), getString(R.string.on_create_view));
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+
+        Activity activity = getActivity();
+        if (activity != null){
+            int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+
+            mEtUsername = v.findViewById(R.id.et_email);
+            mEtPassword = v.findViewById(R.id.et_password);
+            mEtConfirm = v.findViewById(R.id.et_repassword);
+            Button btnAdd = v.findViewById(R.id.btn_register);
+            btnAdd.setOnClickListener(this);
+            //Button btnExit = v.findViewById(R.id.exit_button);
+            if (rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180) {
+              //  btnExit.setOnClickListener(this);
+            }
+            else {
+               // btnExit.setVisibility(View.GONE);
+            }
+        }
+
+        return v;
+
     }
+
+    @Override
+    public void onClick(View view){
+        switch (view.getId()) {
+            case R.id.btn_register:
+                createAccount();
+                break;
+            /*
+            case R.id.exit_button:
+                FragmentActivity activity = getActivity();
+                if (activity != null) {
+                    activity.getSupportFragmentManager().popBackStack();
+                }
+
+             */
+        }
+    }
+
+    private void createAccount() {
+        FragmentActivity activity = getActivity();
+        String username = mEtUsername.getText().toString();
+        String password = mEtPassword.getText().toString();
+        String confirm = mEtConfirm.getText().toString();
+        if (activity != null) {
+            if (password.equals(confirm) && !username.equals("") && !password.equals("")) {
+
+                User account = new User(username, password, username);
+                /////////////////////////////////ADD to Firebase
+                //////////////////////////////////
+                Toast.makeText(activity.getApplicationContext(), "Sign up successfully.", Toast.LENGTH_SHORT).show();
+            } else if ((username.equals("")) || (password.equals("")) || (confirm.equals(""))) {
+                Toast.makeText(activity.getApplicationContext(), "Username or Password cannot be empty", Toast.LENGTH_SHORT).show();
+            } else if (!password.equals(confirm)){
+                Toast.makeText(activity.getApplicationContext(), "Password not match. Please enter again", Toast.LENGTH_SHORT).show();
+                mEtUsername.setText("");
+                mEtPassword.setText("");
+                mEtConfirm.setText("");
+            }
+
+
+            else {
+
+                ///////////////////////////Here is to show the error message
+
+            }
+        }
+    }
+
 
 }
