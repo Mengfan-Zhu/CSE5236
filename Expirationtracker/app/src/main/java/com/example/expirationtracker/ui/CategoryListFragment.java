b3ea4de0b9f44e7d85e703d52b75f4251c7a6025
Mengfan Zhu <zhu.2420@osu.edu>
2020-03-03 15:32:41 -0500

    add item list

--- Expirationtracker/app/src/main/java/com/example/expirationtracker/ui/CategoryListFragment.java
+++ Expirationtracker/app/src/main/java/com/example/expirationtracker/ui/CategoryListFragment.java
@@ -1,8 +1,8 @@
 package com.example.expirationtracker.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import androidx.fragment.app.Fragment;
 
@@ -29,10 +29,8 @@ import com.google.firebase.database.ValueEventListener;
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  * to handle interaction events.
- * Use the {@link CategoryListFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
-public class CategoryListFragment extends Fragment {
+public class CategoryListFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //    private static final String ARG_PARAM1 = "param1";
@@ -51,38 +49,12 @@ public class CategoryListFragment extends Fragment {
         // Required empty public constructor
     }
 
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment CategoryListFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static CategoryListFragment newInstance(String param1, String param2) {
-//        CategoryListFragment fragment = new CategoryListFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         // TODO: Need to load all categories from database.
         mView = inflater.inflate(R.layout.fragment_category_list, container, false);
         mAuth = FirebaseAuth.getInstance();
@@ -91,6 +63,7 @@ public class CategoryListFragment extends Fragment {
         //if(addButton != null){
         //
         //}
+
         mCategoryReference = FirebaseDatabase.getInstance().getReference().child("categories").child(mAuth.getUid());
         Query categoryQuery = mCategoryReference.orderByChild("name");
 
@@ -104,36 +77,64 @@ public class CategoryListFragment extends Fragment {
                 categoryLayout.setOrientation(LinearLayout.VERTICAL);
                 categoryList.addView(categoryLayout);
                 // add each layout
-                for (DataSnapshot currentSnapshot: dataSnapshot.getChildren()) {
+                for (DataSnapshot currentSnapshot : dataSnapshot.getChildren()) {
+                    final String categoryId = currentSnapshot.getKey();
                     Category category = currentSnapshot.getValue(Category.class);
+                    // linearLayout for one category content
                     LinearLayout categoryContent = new LinearLayout(activity);
                     categoryContent.setOrientation(LinearLayout.VERTICAL);
                     categoryContent.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
                     categoryContent.setDividerPadding(10);
                     categoryContent.setBackgroundResource(R.drawable.bg_item);
+                    categoryContent.setClickable(true);
+                    categoryContent.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getActivity(), ItemListActivity.class);
+                            intent.putExtra("categoryId",categoryId);
+                            startActivity(intent);
+                        }
+                    });
+                    // TextView for name
                     TextView name = new TextView(activity);
                     name.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
                     name.setText(category.getName());
-                    name.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
+                    name.setTextSize(TypedValue.COMPLEX_UNIT_SP, 25);
                     categoryContent.addView(name);
+                    // TextView for contents
                     TextView contents = new TextView(activity);
                     contents.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-                    contents.setText("Begin: "+ category.getBegin()+"\nFrequency: " + category.getFrequency()+"\nTime: "+ category.getTime());
+                    contents.setText("Begin: " + category.getBegin() + "\nFrequency: " + category.getFrequency() + "\nTime: " + category.getTime());
                     categoryContent.addView(contents);
+                    // linearLayout for buttons
                     LinearLayout buttonsLayout = new LinearLayout(activity);
                     buttonsLayout.setOrientation(LinearLayout.HORIZONTAL);
                     buttonsLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    // edit button
                     Button editButton = new Button(activity);
                     editButton.setText("Edit");
                     editButton.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    editButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            //TODO
+                        }
+                    });
                     buttonsLayout.addView(editButton);
+                    // delete button
                     Button deleteButton = new Button(activity);
                     deleteButton.setText("Delete");
                     deleteButton.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    deleteButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            mCategoryReference.child(categoryId).removeValue();
+                        }
+                    });
                     buttonsLayout.addView(deleteButton);
                     categoryContent.addView(buttonsLayout);
                     categoryLayout.addView(categoryContent);
-              }
+                }
                 // ...
             }
 
@@ -147,49 +148,13 @@ public class CategoryListFragment extends Fragment {
 
         return mView;
     }
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//
-//
-//    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_add_category:
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+
+        }
+    }
 }
