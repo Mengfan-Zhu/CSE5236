10e890507c8624184c0d502dc36a8eb511875453
Yue Zhang <zhyue05@gmail.com>
2020-03-13 23:20:33 -0400

    add nav bar

---
+++ Expirationtracker/app/src/main/java/com/example/expirationtracker/ui/NavActivity.java
@@ -0,0 +1,53 @@
+package com.example.expirationtracker.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.expirationtracker.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class NavActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
+
+    }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_category:
+                            openFragment(CategoryListFragment.newInstance());
+                            return true;
+                        case R.id.navigation_add:
+                            openFragment(AddFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_setting:
+                            openFragment(SettingFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
+}
